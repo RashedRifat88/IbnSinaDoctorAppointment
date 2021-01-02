99ac8c2966210a854975cf1b20ff397f48f3f858
Rifat <rasrif88@gmail.com>
2021-01-02 20:35:22 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ibnsinadoctorappointment/ui/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ibnsinadoctorappointment.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ibnsinadoctorappointment.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
